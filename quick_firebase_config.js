2ced73e3283d823c94e1dc0b3add1401c31a6258
quickhelpai2026-star <quick.helpai2026@gmail.com>
2026-06-17 22:24:17 -0500

    Add files via upload

---
+++ quick_firebase_config.js
@@ -0,0 +1,14 @@
+window.QUICK_FIREBASE_CONFIG = {
+  enabled: true,
+  collection: "quick_lastmile",
+  documentId: "platform_state",
+  firebaseConfig: {
+    apiKey: "AIzaSyDYPV_JZiZ8cyT_3vbzDKkkAaMIrjh1XzI",
+    authDomain: "gestion-administrativa-60773.firebaseapp.com",
+    projectId: "gestion-administrativa-60773",
+    storageBucket: "gestion-administrativa-60773.firebasestorage.app",
+    messagingSenderId: "26074206544",
+    appId: "1:26074206544:web:f5443068270bc0c9d09c55",
+    measurementId: "G-19NBTVRQ9W"
+  }
+};
